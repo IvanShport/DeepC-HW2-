b8e2ce59cbdcbc4d3e88333b79915fed41fadbf4
IvanShport <ivan.shport.98@mail.ru>
2018-10-03 14:03:25 +0300

    [+] add calcDivisionWIthLongNumber

--- main.c
+++ main.c
@@ -11,15 +11,25 @@ typedef struct LongNumber {
     int sign;
 }LongNumber;
 
-LongNumber* initLongNumber(){
+LongNumber* initLongNumber(size_t size){
     LongNumber* LN = (LongNumber*)malloc(sizeof(LongNumber));
     if(LN == NULL) {
         printf("[error]\n");
         return NULL;
     }
-    LN->arrayOfNumber = NULL;
-    LN->size = 0;
+
+    LN->size = size;
     LN->sign = 1;
+    LN->arrayOfNumber = NULL;
+
+    if (size != 0) {
+        LN->arrayOfNumber = (int *) malloc(LN->size * sizeof(int));
+        if (LN->arrayOfNumber == NULL) {
+            printf("[error]\n");
+            free(LN);
+            return NULL;
+        }
+    }
 
     return LN;
 }
@@ -47,6 +57,11 @@ LongNumber* calcMultiplicationWIthString(char* leftValue, size_t leftSize, char*
 //----------------------------------------------------------------------------------------------------------------------
 LongNumber* calcSubtractionWIthString(char* leftValue, size_t leftSize, char* rightValue, size_t rightSize);
 //----------------------------------------------------------------------------------------------------------------------
+LongNumber* shift(LongNumber* value, int difference);
+void inputZero(LongNumber* value);
+LongNumber* calcDivisionWIthLongNumber(LongNumber* leftValue, LongNumber* rightValue);
+LongNumber* calcDivisionWIthString(char* leftValue, size_t leftSize, char* rightValue, size_t rightSize);
+//----------------------------------------------------------------------------------------------------------------------
 char* input_row();
 
 int main(int argc, char const *argv[]) {
@@ -54,7 +69,7 @@ int main(int argc, char const *argv[]) {
     char* leftValue = input_row();
     char* rightValue = input_row();
 
-    LongNumber* result = calcMultiplicationWIthString(leftValue, strlen(leftValue), rightValue, strlen(rightValue));
+    LongNumber* result = calcDivisionWIthString(leftValue, strlen(leftValue), rightValue, strlen(rightValue));
     if(result == NULL) {
         free(leftValue);
         free(rightValue);
@@ -112,29 +127,25 @@ char* input_row(){
 LongNumber* calcAdditionWIthLongNumber(LongNumber* leftValue, LongNumber* rightValue) {
     size_t resultSize = rightValue->size > leftValue->size ? rightValue->size + 1 : leftValue->size + 1;
 
-    LongNumber* resultLN = initLongNumber();
+    LongNumber* resultLN = initLongNumber(resultSize);
     if(resultLN == NULL) {
         return NULL;
     }
-    resultLN->arrayOfNumber = (int*)malloc(resultSize * sizeof(int));
-    if(resultLN->arrayOfNumber == NULL) {
-        printf("[error]\n");
-        free(resultLN);
-        freeLongNumber(leftValue);
-        freeLongNumber(rightValue);
-        return NULL;
-    }
-    resultLN->size = resultSize;
     resultLN->sign = leftValue->sign;
 
-    for (size_t i = 0; i < resultLN->size; i++) {
-        resultLN->arrayOfNumber[i] += leftValue->arrayOfNumber[i] + rightValue->arrayOfNumber[i];
+    int leftBuffer = 0;
+    int rightBuffer = 0;
+
+    for (size_t i = 0; i < resultLN->size - 1; i++) {
+        leftBuffer = leftValue->size > i ? leftValue->arrayOfNumber[i] : 0;
+        rightBuffer = rightValue->size > i ? rightValue->arrayOfNumber[i] : 0;
+
+        resultLN->arrayOfNumber[i] += leftBuffer + rightBuffer;
         resultLN->arrayOfNumber[i + 1] += resultLN->arrayOfNumber[i] / 10;
         resultLN->arrayOfNumber[i] %= 10;
     }
 
-    freeLongNumber(leftValue);
-    freeLongNumber(rightValue);
+    deleteZero(resultLN);
 
     return resultLN;
 }
@@ -163,12 +174,17 @@ LongNumber* calcAdditionWIthString(char* leftValue, size_t leftSize, char* right
         if (rightLN->sign < 0) {
             resultLN = calcSubtractionWIthLongNumber(leftLN, rightLN);
         }
-
     }
 
-    deleteZero(resultLN);
-    reversLongNumber(resultLN);
+    if(resultLN == NULL){
+        freeLongNumber(leftLN);
+        freeLongNumber(rightLN);
+        return NULL;
+    }
 
+    reversLongNumber(resultLN);
+    freeLongNumber(leftLN);
+    freeLongNumber(rightLN);
     return resultLN;
 }
 
@@ -177,19 +193,10 @@ LongNumber* calcAdditionWIthString(char* leftValue, size_t leftSize, char* right
 LongNumber* calcMultiplicationWIthLongNumber(LongNumber* leftValue, LongNumber* rightValue) {
     size_t resultSize = rightValue->size + leftValue->size + 1;
 
-    LongNumber* resultLN = initLongNumber();
+    LongNumber* resultLN = initLongNumber(resultSize);
     if(resultLN == NULL) {
         return NULL;
     }
-    resultLN->arrayOfNumber = (int*)malloc(resultSize * sizeof(int));
-    if(resultLN->arrayOfNumber == NULL) {
-        printf("[error]\n");
-        free(resultLN);
-        freeLongNumber(leftValue);
-        freeLongNumber(rightValue);
-        return NULL;
-    }
-    resultLN->size = resultSize;
     resultLN->sign = leftValue->sign * rightValue->sign;
 
     for (size_t i = 0; i < leftValue->size; i++) {
@@ -203,8 +210,8 @@ LongNumber* calcMultiplicationWIthLongNumber(LongNumber* leftValue, LongNumber* 
         resultLN->arrayOfNumber[i] %= 10;
     }
 
-    freeLongNumber(leftValue);
-    freeLongNumber(rightValue);
+    deleteZero(resultLN);
+    reversLongNumber(resultLN);
 
     return resultLN;
 }
@@ -214,19 +221,20 @@ LongNumber* calcMultiplicationWIthString(char* leftValue, size_t leftSize, char*
     if(leftLN == NULL) {
         return NULL;
     }
-    LongNumber* rightLN = createLongNumberFromString(rightValue, rightSize, 1);
+    LongNumber* rightLN = createLongNumberFromString(rightValue, rightSize, -1);
     if(rightLN == NULL) {
         freeLongNumber(leftLN);
         return NULL;
     }
     LongNumber* resultLN = calcMultiplicationWIthLongNumber(leftLN, rightLN);
     if(resultLN == NULL){
+        freeLongNumber(leftLN);
+        freeLongNumber(rightLN);
         return NULL;
     }
 
-    deleteZero(resultLN);
-    reversLongNumber(resultLN);
-
+    freeLongNumber(leftLN);
+    freeLongNumber(rightLN);
     return resultLN;
 }
 
@@ -237,22 +245,28 @@ LongNumber* calcSubtractionWIthLongNumber(LongNumber* leftValue, LongNumber* rig
         LongNumber* tempLN = leftValue;
         leftValue = rightValue;
         rightValue = tempLN;
-        leftValue->sign = -1;
+        leftValue->sign *= -1;
     }
+
+    LongNumber* resultLN = initLongNumber(leftValue->size);
+    if(resultLN == NULL) {
+        return NULL;
+    }
+    resultLN->sign = leftValue->sign;
 
     int buffer = 0;
     int rightBuffer = 0;
 
-    for (size_t i = 0; i < leftValue->size; i++) {
+    for (size_t i = 0; i < resultLN->size; i++) {
         rightBuffer = rightValue->size > i ? rightValue->arrayOfNumber[i] : 0;
         buffer = leftValue->arrayOfNumber[i] - rightBuffer - buffer;
-        leftValue->arrayOfNumber[i] = (buffer + 10) % 10;
+        resultLN->arrayOfNumber[i] = (buffer + 10) % 10;
         buffer = buffer < 0 ? 1 : 0;
     }
 
-    freeLongNumber(rightValue);
+    deleteZero(resultLN);
 
-    return leftValue;
+    return resultLN;
 }
 
 LongNumber* calcSubtractionWIthString(char* leftValue, size_t leftSize, char* rightValue, size_t rightSize) {
@@ -260,7 +274,7 @@ LongNumber* calcSubtractionWIthString(char* leftValue, size_t leftSize, char* ri
     if (leftLN == NULL) {
         return NULL;
     }
-    LongNumber *rightLN = createLongNumberFromString(rightValue, rightSize, 1);
+    LongNumber *rightLN = createLongNumberFromString(rightValue, rightSize, -1);
     if (rightLN == NULL) {
         freeLongNumber(leftLN);
         return NULL;
@@ -272,16 +286,140 @@ LongNumber* calcSubtractionWIthString(char* leftValue, size_t leftSize, char* ri
         if (leftLN->sign == 1) {
             resultLN = calcSubtractionWIthLongNumber(leftLN, rightLN);
         } else {
+            rightLN->sign *= -1;
             resultLN = calcSubtractionWIthLongNumber(rightLN, leftLN);
         }
     } else {
         resultLN = calcAdditionWIthLongNumber(leftLN, rightLN);
+
+    }
+
+    if(resultLN == NULL){
+        freeLongNumber(leftLN);
+        freeLongNumber(rightLN);
+        return NULL;
+    }
+
+    reversLongNumber(resultLN);
+    freeLongNumber(rightLN);
+    freeLongNumber(leftLN);
+    return resultLN;
+}
+
+//----------------------------------------------------------------------------------------------------------------------
+
+LongNumber* shift(LongNumber* value, int difference) {
+    LongNumber* resultLN = initLongNumber(0);
+    if (resultLN == NULL) {
+        return NULL;
+    }
+    resultLN->size = value->size + difference;
+    resultLN->arrayOfNumber = (int*)calloc(resultLN->size, sizeof(int));
+    if(resultLN->arrayOfNumber == NULL) {
+        printf("[error]\n");
+        free(resultLN);
+        return NULL;
+    }
+//    memcpy(resultLN->arrayOfNumber, value->arrayOfNumber, value->size);
+
+    for (size_t i = 0; i < value->size; i++) {
+        resultLN->arrayOfNumber[i] = value->arrayOfNumber[i];
+    }
+    freeLongNumber(value);
+    return resultLN;
+}
+
+void inputZero(LongNumber* value) {
+    for (size_t i = 0; i < value->size; i++) {
+        value->arrayOfNumber[i] = 0;
+    }
+    deleteZero(value);
+}
+
+void output(LongNumber* value) {
+    for (size_t i = 0; i < value->size; i++) {
+        printf("%d ", value->arrayOfNumber[i]);
+    }
+    printf("\n");
+}
+
+LongNumber* calcDivisionWIthLongNumber(LongNumber* leftValue, LongNumber* rightValue) {
+    LongNumber* resultLN = initLongNumber(leftValue->size);
+    if(resultLN == NULL) {
+        return NULL;
+    }
+
+    if (calcCompareWIthLongNumber(leftValue, rightValue) == -1) {
+        inputZero(resultLN);
+        return resultLN;
+    }
+
+    resultLN->sign = leftValue->sign * rightValue->sign;
+
+    leftValue->sign = rightValue->sign = 1;
+    reversLongNumber(rightValue);
+
+    int difference = (int)leftValue->size - (int)rightValue->size;
+    rightValue = shift(rightValue, difference);
+
+    LongNumber* tempLN = initLongNumber(leftValue->size + 1);
+    if(tempLN == NULL) {
+        freeLongNumber(resultLN);
+        return NULL;
+    }
+
+    reversLongNumber(rightValue);
+    int digit = 0;
+
+    for (; difference >= 0; difference--) {
+        inputZero(tempLN);
+        digit = 0;
+
+        do {
+            tempLN = calcAdditionWIthLongNumber(tempLN, rightValue);
+            digit++;
+        } while(calcCompareWIthLongNumber(tempLN, leftValue) <= 0);
+
+        tempLN = calcSubtractionWIthLongNumber(tempLN, rightValue);
+        digit--;
+
+        leftValue = calcSubtractionWIthLongNumber(leftValue, tempLN);
+        resultLN->arrayOfNumber[difference] = digit;
+
+        reversLongNumber(rightValue);
+        rightValue->size--;
+        reversLongNumber(rightValue);
     }
 
     deleteZero(resultLN);
     reversLongNumber(resultLN);
+    freeLongNumber(tempLN);
 
     return resultLN;
+}
+
+LongNumber* calcDivisionWIthString(char* leftValue, size_t leftSize, char* rightValue, size_t rightSize) {
+    LongNumber *leftLN = createLongNumberFromString(leftValue, leftSize, 1);
+    if (leftLN == NULL) {
+        return NULL;
+    }
+    LongNumber *rightLN = createLongNumberFromString(rightValue, rightSize, 1);
+    if (rightLN == NULL) {
+        freeLongNumber(leftLN);
+        return NULL;
+    }
+
+    LongNumber* resultLN = calcDivisionWIthLongNumber(leftLN, rightLN);
+    if(resultLN == NULL){
+        freeLongNumber(leftLN);
+        freeLongNumber(rightLN);
+        return NULL;
+    }
+
+    freeLongNumber(leftLN);
+    freeLongNumber(rightLN);
+    return resultLN;
+
 }
 
 //----------------------------------------------------------------------------------------------------------------------
@@ -305,17 +443,10 @@ int calcCompareWIthLongNumber(LongNumber* leftValue, LongNumber* rightValue) {
 //----------------------------------------------------------------------------------------------------------------------
 
 LongNumber* createLongNumberFromString(char* stringOfNumbers, size_t stringSize, int sign) {
-    LongNumber* LN = initLongNumber();
+    LongNumber* LN = initLongNumber(stringSize);
     if(LN == NULL) {
         return NULL;
     }
-    LN->arrayOfNumber = (int*)malloc(stringSize * sizeof(int));
-    if(LN->arrayOfNumber == NULL) {
-        printf("[error]\n");
-        free(LN);
-        return NULL;
-    }
-    LN->size = stringSize;
     LN->sign = sign;
 
     for (int i = (int)LN->size - 1; i >= 0; i--) {
